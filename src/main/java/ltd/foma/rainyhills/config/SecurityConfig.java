d9c4135f17bc42ceb0898c123e91f5e48bf17844
fomalsd <fomalsd@users.noreply.github.com>
2021-08-26 19:16:21 +0300

    Changed password encoder to BCrypt

--- src/main/java/ltd/foma/rainyhills/config/SecurityConfig.java
+++ src/main/java/ltd/foma/rainyhills/config/SecurityConfig.java
@@ -1,16 +1,25 @@
 package ltd.foma.rainyhills.config;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.lang.invoke.MethodHandles;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final Logger log = getLogger( MethodHandles.lookup().lookupClass() );
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,12 +40,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             ;
     }
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Autowired
     public void ConfigureGlobal(AuthenticationManagerBuilder authBuilder) throws Exception {
         authBuilder.inMemoryAuthentication()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .passwordEncoder(getPasswordEncoder())
                 .withUser("user")
-                .password("pass")
+                .password(BCrypt.hashpw("pass",BCrypt.gensalt()))
                 .roles("USER");
     }
 }
